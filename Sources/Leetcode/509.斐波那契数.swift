01ff49c08935879862b2b123f7d20df515504769
misakatao <misakatao@gmail.com>
2023-08-04 16:53:21 +0800

    feat: code push

---
+++ Sources/Leetcode/509.斐波那契数.swift
@@ -0,0 +1,83 @@
+//
+//  509.斐波那契数.swift
+//  swift-algorithm
+//
+//  Created by Misaka on 2023/8/4.
+//
+
+import Foundation
+
+extension Solution {
+    
+    typealias Matrix = [[UInt64]]
+    
+    func matrixMultiply(_ a: Matrix, _ b: Matrix) -> Matrix {
+        let n = a.count
+        let m = b[0].count
+        let p = b.count
+        
+        var result = Matrix(repeating: Array(repeating: 0, count: m), count: n)
+        
+        for i in 0..<n {
+            for j in 0..<m {
+                for k in 0..<p {
+                    result[i][j] += a[i][k] * b[k][j]
+                }
+            }
+        }
+        
+        return result
+    }
+    
+    func matrixPower(_ a: Matrix, _ n: Int) -> Matrix {
+        var result = a
+        var b = a
+        var k = n - 1
+        
+        while k > 0 {
+            if k % 2 != 0 {
+                result = matrixMultiply(result, b)
+            }
+            
+            b = matrixMultiply(b, b)
+            k /= 2
+        }
+        
+        return result
+    }
+    
+    func fib(_ n: Int) -> Int {
+        if n < 2 { return n }
+        
+        let matrix = [[UInt64(1), UInt64(1)], [UInt64(1), UInt64(0)]]
+        let resultMatrix = matrixPower(matrix, n - 1)
+        return Int(resultMatrix[0][0])
+    }
+    
+    func fib1(_ n: Int) -> Int {
+        if n < 2 { return n }
+        
+        var dp_i_1 = 1 // dp[i - 1]
+        var dp_i_2 = 0 // dp[i - 2]
+        for _ in 2...n {
+            // dp[i] = dp[i - 1] + dp[i - 2]
+            let dp_i = dp_i_1 + dp_i_2
+            // 滚动更新
+            dp_i_2 = dp_i_1
+            dp_i_1 = dp_i
+        }
+        return dp_i_1
+    }
+    
+    func fib2(_ n: Int) -> Int {
+        if n < 2 { return n }
+        var dp: [Int] = Array(repeating: 0, count: n + 1)
+        dp[1] = 1 // dp[i - 1]
+        dp[0] = 0 // dp[i - 2]
+        for i in 2...n {
+            dp[i] = dp[i - 1] + dp[i - 2]
+        }
+        return dp[n]
+    }
+    
+}
--- Sources/main.swift
+++ Sources/main.swift
@@ -7,15 +7,61 @@
 
 import Foundation
 
-// 构造二叉树
-let root = TreeNode(1)
-root.left = TreeNode(2)
-root.right = TreeNode(3)
-root.left?.left = TreeNode(4)
-root.left?.right = TreeNode(5)
-root.right?.right = TreeNode(6)
-root.left?.left?.left = TreeNode(7)
+func measureTime(_ closure: () -> Void) -> TimeInterval {
+    let startTime = DispatchTime.now().uptimeNanoseconds
+    closure()
+    let elapsedNanoSeconds = DispatchTime.now().uptimeNanoseconds - startTime
+    return TimeInterval(Double(elapsedNanoSeconds) / 1_000_000)
+}
 
-// 计算直径
-let result = Solution().diameterOfBinaryTree(root)
-print(result) // 输出 4
+let s = Solution()
+
+func testCase_1() {
+    
+}
+
+func testCase_5() {
+    
+}
+
+func testCase_104() {
+    
+}
+
+func testCase_144() {
+    
+}
+
+func testCase_314() {
+    
+}
+
+func testCase_320() {
+    
+}
+
+func testCase_322() {
+    print("耗时: \(measureTime { print(s.coinChange([186,419,83,408], 6249)) }) ms")
+}
+
+func testCase_509() {
+    print("耗时: \(measureTime { print(s.fib2(65)) }) ms")
+    print("耗时: \(measureTime { print(s.fib1(65)) }) ms")
+    print("耗时: \(measureTime { print(s.fib(65)) }) ms")
+}
+
+func testCase_543() {
+    // 构造二叉树
+    let root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left?.left = TreeNode(4)
+    root.left?.right = TreeNode(5)
+    root.right?.right = TreeNode(6)
+    root.left?.left?.left = TreeNode(7)
+    
+    // 计算直径
+    print("耗时: \(measureTime { print(s.diameterOfBinaryTree(root)) }) ms")
+}
+
+
