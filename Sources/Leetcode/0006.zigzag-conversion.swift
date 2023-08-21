85f56b37e98bd11405800844a18a76ddef1788cc
Misaka <misakatao@gmail.com>
2023-08-21 17:58:02 +0800

    feat: push code

---
+++ Sources/Leetcode/0006.zigzag-conversion.swift
@@ -0,0 +1,28 @@
+//
+//  swift-algorithm
+//
+//  Created by Misaka on 2023/8/18.
+//
+//  N 字形变换
+
+import Foundation
+
+extension Solution {
+    /*
+     将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
+
+     比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下：
+
+     P   A   H   N
+     A P L S I I G
+     Y   I   R
+     之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。
+
+     请你实现这个将字符串进行指定行数变换的函数：
+
+     string convert(string s, int numRows);
+     */
+    func convert(_ s: String, _ numRows: Int) -> String {
+        return ""
+    }
+}
