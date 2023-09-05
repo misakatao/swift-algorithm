da0708b29b0de9063036ab6524586592a2f2f3d9
Misaka <misakatao@gmail.com>
2023-09-05 10:47:02 +0800

    feat: push code

---
+++ Sources/Leetcode/0096.unique-binary-search-trees.swift
@@ -0,0 +1,18 @@
+//
+//  swift-algorithm
+//
+//  Created by Misaka on 2023/9/1.
+//
+//  不同的二叉搜索树
+
+import Foundation
+
+extension Solution {
+    /*
+     给你一个整数 n ，求恰由 n 个节点组成且节点值从 1 到 n 互不相同的 二叉搜索树 有多少种？返回满足题意的二叉搜索树的种数。
+     */
+    func numTrees(_ n: Int) -> Int {
+        // TODO: something
+        return -1
+    }
+}
