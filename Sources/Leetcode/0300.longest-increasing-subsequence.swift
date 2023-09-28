de08719cae4be2309f192686bfe3740dc9f31981
Misaka <misakatao@gmail.com>
2023-09-28 14:09:00 +0800

    feat: push code

---
+++ Sources/Leetcode/0300.longest-increasing-subsequence.swift
@@ -0,0 +1,20 @@
+//
+//  swift-algorithm
+//
+//  Created by Misaka on 2023/9/13.
+//
+//  最长递增子序列
+
+import Foundation
+
+extension Solution {
+    /*
+     给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+     
+     子序列 是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+     */
+    func lengthOfLIS(_ nums: [Int]) -> Int {
+        // TODO: something
+        return 0
+    }
+}
---
+++ Sources/Leetcode/2104.sum-of-subarray-ranges.swift
@@ -0,0 +1,22 @@
+//
+//  swift-algorithm
+//
+//  Created by Misaka on 2023/9/15.
+//
+//  子数组范围和
+
+import Foundation
+
+extension Solution {
+    /*
+     给你一个整数数组 nums 。nums 中，子数组的 范围 是子数组中最大元素和最小元素的差值。
+     
+     返回 nums 中 所有 子数组范围的 和 。
+     
+     子数组是数组中一个连续 非空 的元素序列。
+     */
+    func subArrayRanges(_ nums: [Int]) -> Int {
+        // TODO: something
+        return 0
+    }
+}
