25d247499ad7499b1631eeca80730a1678200076
Misaka <misakatao@gmail.com>
2023-12-25 16:26:29 +0800

    feat: push code

--- Package.swift
+++ Package.swift
@@ -40,17 +40,23 @@ let myfiles: [String] = [
     // Leetcode
     /// BinarySearch
     "./Sources/Leetcode/BinarySearch/binary-search.swift",
+    "./Sources/Leetcode/BinarySearch/capacity-to-ship-packages-within-d-days.swift",
     "./Sources/Leetcode/BinarySearch/find-first-and-last-position-of-element-in-sorted-array.swift",
+    "./Sources/Leetcode/BinarySearch/koko-eating-bananas.swift",
+    "./Sources/Leetcode/BinarySearch/split-array-largest-sum.swift",
     "./Sources/Leetcode/BinarySearch/two-sum-ii-input-array-is-sorted.swift",
     
     "./Sources/Leetcode/add-two-numbers.swift",
     "./Sources/Leetcode/binary-search-tree-to-greater-sum-tree.swift",
     "./Sources/Leetcode/insert-into-a-binary-search-tree.swift",
     "./Sources/Leetcode/kth-largest-element-in-an-array.swift",
+    "./Sources/Leetcode/lowest-common-ancestor-of-a-binary-tree.swift",
     "./Sources/Leetcode/maximum-binary-tree.swift",
     "./Sources/Leetcode/minimum-jumps-to-reach-home.swift",
     "./Sources/Leetcode/partition-equal-subset-sum.swift",
     "./Sources/Leetcode/search-in-a-binary-search-tree.swift",
+    "./Sources/Leetcode/sort-an-array.swift",
+    "./Sources/Leetcode/super-egg-drop.swift",
     "./Sources/Leetcode/two-sum.swift",
     "./Sources/Leetcode/unique-binary-search-trees-ii.swift",
     "./Sources/Leetcode/unique-binary-search-trees.swift",
@@ -74,7 +80,6 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-collections", branch: "release/1.1")
     ],
     targets: [
-        .target(name: "Leetcode", path: "Leetcode"),
         .target(name: "Utils", dependencies: [.product(name: "Collections", package: "swift-collections")], path: "Sources/Utils"),
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
